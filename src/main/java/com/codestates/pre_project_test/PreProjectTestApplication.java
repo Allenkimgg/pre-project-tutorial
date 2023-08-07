4a054ada123f99b2428e2b5e707064a9fa42256e
Allenkimgg <129909760+Allenkimgg@users.noreply.github.com>
2023-08-07 14:43:23 +0900

    initial

---
+++ src/main/java/com/codestates/pre_project_test/PreProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.pre_project_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/pre_project_test/PreProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.pre_project_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
